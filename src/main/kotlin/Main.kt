d68fb3c34aecf612cad74e0d46db6e926a319c2b
MegaHamer <135736897+MegaHamer@users.noreply.github.com>
2023-10-07 10:28:00 +0300

    2 задание (не проверил)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,52 @@
 fun main() {
-    oneTask()
+    //oneTask()
+    twoTask()
+}
+
+fun twoTask(){
+    print("Введите слово для шифровки: ")
+    var text = readln().toString().replace(Regex("[0123456789]")){
+        when (it.value){
+            "0","1","2","3","4","5","6","7","8","9" -> ""
+            else -> it.value
+        }
+    }
+    text = text.uppercase()
+    fun shifrowka (A:Char, B:Char, C:Char):Int{
+        var azb:MutableSet<Char> = (('А'..'И')+('К'..'Я')+' ').toMutableSet()
+        var aInt:Int
+        var bInt:Int
+        if (A == 'Й'){
+            aInt = azb.indexOf('И')
+        }
+        else
+            if (A==' '){
+                aInt = azb.indexOf(C)
+            }
+        else
+            aInt = azb.indexOf(A)
+        if (B == 'Й'){
+            bInt = azb.indexOf('И')
+        }
+        else
+            if (B==' '){
+                aInt = azb.indexOf(C)
+            }
+        else
+            bInt = azb.indexOf(B)
+        return (aInt+1)*31+(aInt+1)
+    }
+    if (text.count()%2 == 1){
+        text+=" "
+    }
+    var shifr:String="";
+    print("Ввести доп символ: ")
+    val sim:Char=readln().uppercase().toCharArray().first()
+    for (i in 0 until text.count() step 2){
+        shifr+="${shifrowka(text[i],text[i+1],sim)} "
+    }
+    println(text)
+    println(shifr)
 }
 fun oneTask(){
     fun playerChoice():Int{
